6706b0d7f0f599d4e27cab8deddaa093e86eee5c
SAURABH BASU <37951940+saurabhbasu07@users.noreply.github.com>
2020-02-03 22:09:02 +0530

    Add files via upload

---
+++ binaryLED.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
